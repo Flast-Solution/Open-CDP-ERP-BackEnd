703c45702278e14a068ff1790dad5a301389095b
LongHuu <long.huu.100@gmail.com>
2025-11-04 22:28:20 +0700

    rename sku in detail

--- src/main/java/vn/flast/dao/UserProfileDao.java
+++ src/main/java/vn/flast/dao/UserProfileDao.java
@@ -20,15 +20,11 @@ package vn.flast.dao;
 /* có trách nghiệm                                                        */
 /**************************************************************************/
 
-import vn.flast.models.User;
 import vn.flast.models.UserProfile;
 import java.util.List;
-import java.util.Set;
 
 public interface UserProfileDao {
     List<UserProfile> findAll();
-    UserProfile findByType(String type);
     UserProfile findById(int id);
-    Set<User> findUserByRole(String role);
     void updateLinkProfile(List<Integer> listPermissions, int userId);
 }
--- src/main/java/vn/flast/dao/UserProfileDaoImpl.java
+++ src/main/java/vn/flast/dao/UserProfileDaoImpl.java
@@ -21,26 +21,15 @@ package vn.flast.dao;
 /**************************************************************************/
 
 import org.springframework.stereotype.Repository;
-import vn.flast.models.User;
 import vn.flast.models.UserProfile;
 import vn.flast.utils.EntityQuery;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 @Repository("userProfileDao")
 public class UserProfileDaoImpl extends DaoImpl<Long, UserProfile> implements UserProfileDao {
 
     public UserProfileDaoImpl() {
         super(UserProfile.class);
-    }
-
-    @Override
-    public UserProfile findByType(String type) {
-        return EntityQuery.create(entityManager, UserProfile.class).stringEqualsTo("type", type)
-                .uniqueResult();
     }
 
     @Override
@@ -55,20 +44,14 @@ public class UserProfileDaoImpl extends DaoImpl<Long, UserProfile> implements Us
     }
 
     @Override
-    public Set<User> findUserByRole(String role) {
-        UserProfile userProfile = findByType(role);
-        return Objects.isNull(userProfile) ? new HashSet<>() : userProfile.getUsers();
-    }
-
-    @Override
     public void updateLinkProfile(List<Integer> listPermissions, int userId) {
         if(listPermissions.isEmpty() || userId == 0) {
             return;
         }
-        entityManager.createNativeQuery("delete from `user_link_profile` where `user_id` =:uId").setParameter("uId", userId).executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM `user_link_profile` WHERE `user_id` =:uId").setParameter("uId", userId).executeUpdate();
         entityManager.flush();
         listPermissions.forEach(item -> {
-            String strQ = "insert into `user_link_profile` (`user_id`, `user_profile_id`) values (%s, %s)".formatted(userId, item);
+            String strQ = "INSERT INTO `user_link_profile` (`user_id`, `user_profile_id`) VALUES (%s, %s)".formatted(userId, item);
             entityManager.createNativeQuery(strQ).executeUpdate();
         });
     }
